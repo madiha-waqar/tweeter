0b42a2eac43cefbae9995abed90ab02234911aff
madiha-waqar <mail2madihawaqar@gmail.com>
2023-06-14 23:52:54 -0400

    Clean code by removing console logs

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -5,17 +5,8 @@ $(document).ready(() => {
   $('.new-tweet textarea').on('input', function (e) {
 
     const numOfChar = $(this).val().length
-    console.log(numOfChar);
     const remainingChars = 140 - numOfChar;
-    console.log(remainingChars);
-
     const counter = $(this).parent().siblings().children()[1];
-    console.log(counter);
-
-    console.log(counter.innerHTML);
     counter.innerHTML = remainingChars
-    /*console.log(counter.innerHTML.remainingChars);*/
-    /*console.log(counter.text.remainingChars);*/
-
   })
 })  
