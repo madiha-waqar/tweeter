6bed2cd43d24683ea1071b551a0329ea57819f49
madiha-waqar <mail2madihawaqar@gmail.com>
2023-06-15 23:42:21 -0400

    Implement function to insert new tweet with JQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,14 +1,7 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
 // Fake data taken from initial-tweets.json
-$(document).ready(function () {
+$(document).ready(function() {
 
-  const tweetdata = [
-    {
+  const tweetdata = [{
       "user": {
         "name": "Newton",
         "avatars": "https://i.imgur.com/73hZDYK.png",
@@ -31,19 +24,40 @@ $(document).ready(function () {
       "created_at": 1461113959088
     }
   ]
-  
-  //const createTweetElement = function (tweetObj) {
-    const $tweet = $(`<article class="tweet">Hello world</article>`);
-    $('.container').append($tweet); 
-    return $tweet;
- // }
 
-  const renderTweets = function (tweetObjArr) {
+  const createTweetElement = function(tweetObj) {
+    const $tweet = $(`
+        <article class="tweet">
+          <header>
+            <div>
+              <img src="${tweetObj.user.avatars}" alt="tweeter-user-icon">
+              <p>${tweetObj.user.name}</p>
+            </div>
+            <p>${tweetObj.user.handle}</p>
+          </header>
+          <p>${tweetObj.content.text}</p>
+          <footer>
+            <p>${tweetObj.created_at}</p>
+            <p>
+              <i class="fa-solid fa-flag"></i>
+              <i class="fa-solid fa-retweet"></i>
+              <i class="fa-solid fa-heart"></i>
+            </p>
+          </footer>
+        </article>
+    `);
+    $('.all-tweets').append($tweet);
+    console.log($tweet);
+    return $tweet;
+ }
+
+  const renderTweets = function(tweetObjArr) {
     // loops through tweets
+    createTweetElement(tweetObjArr[1]);
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
   }
 
-  renderTweets(data);
+  renderTweets(tweetdata);
 
 });
