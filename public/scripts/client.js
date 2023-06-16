84fa31575e51e5292edb94c4ab2e3b6330d5eef4
madiha-waqar <mail2madihawaqar@gmail.com>
2023-06-16 02:16:37 -0400

    Serialize form data and send it to server as query string

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,31 +1,31 @@
 // Fake data taken from initial-tweets.json
-$(document).ready(function() {
+$(document).ready(function () {
 
   const tweetdata = [{
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
     },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
   ];
 
-  const createTweetElement = function(tweetObj) {
+  const createTweetElement = function (tweetObj) {
     const $tweet = $(`
         <article class="tweet">
           <header>
@@ -47,19 +47,29 @@ $(document).ready(function() {
         </article>
     `);
     return $tweet;
- }
+  }
 
-  const renderTweets = function(tweetObjArr) {
+  const renderTweets = function (tweetObjArr) {
     for (const tweet of tweetObjArr) {
-    const $tweet = createTweetElement(tweet);
-    $('.all-tweets').append($tweet);
+      const $tweet = createTweetElement(tweet);
+      $('.all-tweets').append($tweet);
     }
   }
 
   renderTweets(tweetdata);
 
-  $('new-tweet form').submit(event => {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', function (event) {
     event.preventDefault();
-  })
+
+    const serializedData = $(this).serialize();
+    $.post('/tweets', serializedData)
+      .then(serializedData => {
+        console.log(serializedData);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  });
 
 });
