34d174246a9e21b343c6b481d4fc177bd96f07fe
madiha-waqar <mail2madihawaqar@gmail.com>
2023-06-15 20:51:39 -0400

    Code cleanup, add comments, fix style errors

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,20 @@
 // When DOM loads it is ready to execute JavaScript code
 $(document).ready(() => {
-  console.log("DOM is ready")
 
+  // Character counter updates on text input
   $('.new-tweet textarea').on('input', function (e) {
 
-    const numOfChar = $(this).val().length
+    // Get character count of input in the textarea
+    const numOfChar = $(this).val().length;
+
+    // Get remaining characters count out of 140 allowed characters
     const remainingChars = 140 - numOfChar;
 
+    // Traverse throught the DOM tree to get to counter class
     const charCounter = $(this).parent().siblings().children('.counter');
     charCounter.text(remainingChars);
 
+    // Turn character count red if user's entry goes over allowed character limit
     if (remainingChars < 0) {
       charCounter.addClass('over-limit');
 
