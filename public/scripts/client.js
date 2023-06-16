a5072f6db4cf1d1f7ca7f55e93b9887cbf1c2015
madiha-waqar <mail2madihawaqar@gmail.com>
2023-06-15 22:24:49 -0400

    Test dynamic tweet dummy data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,46 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Fake data taken from initial-tweets.json
+$(document).ready(function () {
+
+  const tweetdata = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+  
+  //const createTweetElement = function (tweetObj) {
+    const $tweet = $(`<article class="tweet">Hello world</article>`);
+    $('.container').append($tweet); 
+    return $tweet;
+ // }
+
+  const renderTweets = function (tweetObjArr) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+  }
+
+  renderTweets(data);
+
+});
